e5ec9fd1365acc3a490a6b363dfe8d9cb16337df
Aurora-YJ <youssefjaouhar911@gmail.com>
2025-10-22 11:00:34 +0100

    get the api from the server

--- backend/src/main/java/com/RunController/RunController.java
+++ backend/src/main/java/com/RunController/RunController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
-
 public class RunController {
 
     @GetMapping("/")
@@ -13,9 +11,9 @@ public class RunController {
         return "Welcome to my Spring Boot API!";
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/api/message")
     public String sayHello() {
-        return "Hello from RunController!";
+        return "Hello from RunController server!";
     }
 
 
--- frontend/my-app/src/app/app.ts
+++ frontend/my-app/src/app/app.ts
@@ -1,23 +1,11 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { MessageService } from './message';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
   imports: [RouterModule],
-  template: `<router-outlet></router-outlet>`,
+   template: `<router-outlet></router-outlet>`
 })
 
-export class App implements OnInit {
-  message = signal('');
-
-  constructor(private messageService: MessageService) {}
-
-  ngOnInit() {
-    this.messageService.getMessage().subscribe({
-      next: (data) => this.message.set(data),
-      error: (err) => console.error('Error:', err),
-    });
-  }
-}
+export class App {}
