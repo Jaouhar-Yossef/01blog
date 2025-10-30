24f12b2548ea85e24c955b0772ddda46ad8464eb
Yossf-jaouhar <jwhryosf@gmail.com>
2025-10-30 15:05:15 +0100

    style

--- frontend/my-app/src/app/auth/auth.ts
+++ frontend/my-app/src/app/auth/auth.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from '../register/register';
 import { LoginComponent } from '../login/login';
@@ -10,10 +11,28 @@ import { LoginComponent } from '../login/login';
   templateUrl: './auth.html',
   styleUrls: ['./auth.css'],
 })
-export class AuthComponent {
+export class AuthComponent implements AfterViewInit {
   showLogin = false;
 
   toggleForm() {
     this.showLogin = !this.showLogin;
+  }
+
+  @ViewChild('bgVideo') bgVideo!: ElementRef<HTMLVideoElement>;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit() {
+
+    if (isPlatformBrowser(this.platformId)) {
+      const video = this.bgVideo.nativeElement as HTMLVideoElement;
+
+      if (video && typeof video.play === 'function') {
+        video.muted = true;
+        video.play().catch(err => console.warn('Autoplay blocked:', err));
+      } else {
+        console.warn('Video element not ready or not a real <video> tag');
+      }
+    }
   }
 }
