d725d35c0e8dd6c4bd9fd1599567c28744c6f8e7
Yossf-jaouhar <jwhryosf@gmail.com>
2025-11-19 17:04:41 +0100

    style

--- frontend/my-app/src/app/navbar/navbar.ts
+++ frontend/my-app/src/app/navbar/navbar.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Setting } from '../setting/setting'; 
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+   standalone: true,
+  imports: [Setting],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
+
 export class Navbar {
+  
+  showSettings: boolean = false;
+
+  toggleSettings() {
+    this.showSettings = !this.showSettings;
+  }
 
 }
